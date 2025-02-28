abcaa3ca4f4f1a47e6ad1204e0de4985f3070154
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2025-02-28 15:00:44 +0530

    Update 28 Feb 1092. Shortest Common Supersequence .cpp
    fixed memory limit exceed issue

--- 28 Feb 1092. Shortest Common Supersequence .cpp
+++ 28 Feb 1092. Shortest Common Supersequence .cpp
@@ -2,41 +2,48 @@ class Solution {
  public:
   string shortestCommonSupersequence(string str1, string str2) {
     string ans;
-    int i = 0;  // str1's index
-    int j = 0;  // str2's index
+    int i = 0, j = 0;
 
-    for (const char c : lcs(str1, str2)) {
-      // Append the letters that are not part of the LCS.
+    for (const char c : getLCS(str1, str2)) {
       while (str1[i] != c)
         ans += str1[i++];
       while (str2[j] != c)
         ans += str2[j++];
-      // Append the letter of the LCS and match it with str1 and str2.
       ans += c;
-      ++i;
-      ++j;
+      ++i, ++j;
     }
 
-    // Append the remaining letters.
     return ans + str1.substr(i) + str2.substr(j);
   }
 
  private:
-  string lcs(const string& a, const string& b) {
-    const int m = a.length();
-    const int n = b.length();
-    // dp[i][j] := the length of LCS(a[0..i), b[0..j))
-    vector<vector<string>> dp(m + 1, vector<string>(n + 1));
+  string getLCS(const string& a, const string& b) {
+    int m = a.length(), n = b.length();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
+    // Compute LCS length
     for (int i = 1; i <= m; ++i)
       for (int j = 1; j <= n; ++j)
         if (a[i - 1] == b[j - 1])
-          dp[i][j] = dp[i - 1][j - 1] + a[i - 1];
+          dp[i][j] = 1 + dp[i - 1][j - 1];
         else
-          dp[i][j] = dp[i - 1][j].length() > dp[i][j - 1].length()
-                         ? dp[i - 1][j]
-                         : dp[i][j - 1];
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 
-    return dp[m][n];
+    // Reconstruct LCS
+    string lcs;
+    int i = m, j = n;
+    while (i > 0 && j > 0) {
+      if (a[i - 1] == b[j - 1]) {
+        lcs += a[i - 1];
+        --i, --j;
+      } else if (dp[i - 1][j] > dp[i][j - 1]) {
+        --i;
+      } else {
+        --j;
+      }
+    }
+
+    reverse(lcs.begin(), lcs.end());
+    return lcs;
   }
 };
