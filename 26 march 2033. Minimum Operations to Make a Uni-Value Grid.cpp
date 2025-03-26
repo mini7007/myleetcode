b2e48a8d7cab02281bf645aee319427ad02cdc5d
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2025-03-26 18:53:37 +0530

    26 march updated

--- 26 march 2033. Minimum Operations to Make a Uni-Value Grid.cpp
+++ 26 march 2033. Minimum Operations to Make a Uni-Value Grid.cpp
@@ -1,12 +1,19 @@
 class Solution {
-    public:
-     int minDepth(TreeNode* root) {
-       if (root == nullptr)
-         return 0;
-       if (root->left == nullptr)
-         return minDepth(root->right) + 1;
-       if (root->right == nullptr)
-         return minDepth(root->left) + 1;
-       return min(minDepth(root->left), minDepth(root->right)) + 1;
-     }
-   };
+  public:
+   int minOperations(vector<vector<int>>& grid, int x) {
+     vector<int> arr;
+     for (const vector<int>& row : grid)
+       arr.insert(arr.end(), row.begin(), row.end());
+     if (ranges::any_of(arr, [&](int a) { return (a - arr[0]) % x; }))
+       return -1;
+ 
+     int ans = 0;
+ 
+     nth_element(arr.begin(), arr.begin() + arr.size() / 2, arr.end());
+ 
+     for (const int a : arr)
+       ans += abs(a - arr[arr.size() / 2]) / x;
+ 
+     return ans;
+   }
+ };
